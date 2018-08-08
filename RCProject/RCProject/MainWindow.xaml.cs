bd58817c50d10dd69ce6cfd27caf97a581c64b74
Шаймарданов Салават Мухаметович <  @witz.local>
2018-08-08 13:22:05 +0500

    NewVers

--- RCProject/RCProject/MainWindow.xaml.cs
+++ RCProject/RCProject/MainWindow.xaml.cs
@@ -27,17 +27,13 @@ namespace RCProject
         public MainWindow()
         {
             InitializeComponent();
-
-        }
-
-        private void Click_Butt1(object sender, RoutedEventArgs e)
-        {
-            Download();
+            
         }
        
-        public void Download()
+        private void Click_Butt1(object sender, RoutedEventArgs e)
         {
-            
+            #region Старый эксп участок кода (не использовать)
+            /*
             Excel.Application ApplEx = null;
             Excel.Workbook WorkBookEx = null;
             Excel.Worksheet WorkShEx = null;
@@ -45,8 +41,6 @@ namespace RCProject
             ApplEx.Visible = true;
             ApplEx.Workbooks.Add();
             WorkShEx = (Excel.Worksheet)ApplEx.ActiveSheet;
-            
-
             if (ButDN.SelectedIndex == 0)
             {
                 WorkShEx.Cells[1, 1] = "Vibor0";
@@ -61,23 +55,55 @@ namespace RCProject
             }
             WorkBookEx.Save();
             //WorkShEx.SaveAs(@"C:\Users\Шаймарданов С М\Desktop\PROJECT_VS\work\RC");
-            //MessageBox.Show(ButDN.Text);
+            //MessageBox.Show(ButDN.Text);   */
+            #endregion
         }
-        
         private void Click_Butt2(object sender, RoutedEventArgs e)
         {
             Excel.Application ApplEx = null;
             Excel.Workbook WorkBookEx = null;
             Excel.Worksheet WorkShEx = null;
+            Excel.Sheets excellsheet = null;
             ApplEx = new Excel.Application();
             ApplEx.Visible = true;
-            ApplEx.Workbooks.Open(@"\\WITZENMANN\!Witz17\!Чтение\!РЦпроект\РЦзаявка.xlsm",
+            WorkBookEx = ApplEx.Workbooks.Open(@"C:\Users\mercu\Desktop\Project VS\WORK\RCProject\RCProject\ЗаявкаРЦ.xlsx",
                 Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
                 Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            excellsheet = WorkBookEx.Worksheets;
+            WorkShEx = (Excel.Worksheet)excellsheet.get_Item(1);
             //\Users\Шаймарданов С М\Desktop\!!!файлы РЦзаявка\!!!файлы РЦзаявка МР\
+            if(TypeConnect.SelectedItem.ToString().Remove(0, 38) == "П12")
+            {
+                WorkShEx.Cells[3, "H"] = customer.Text;
+                WorkShEx.Cells[3, "J"] = ProjectCRM.Text;
+                WorkShEx.Cells[3, "K"] = "1";
+                WorkShEx.Cells[3, "L"] = SupervSubject.SelectedItem.ToString().Remove(0,38);
+                if(CustLabeling.IsEnabled == true)
+                {
+                    WorkShEx.Cells[3, "M"] = CustLabeling.Text;
+                }
+                else
+                {
+                    WorkShEx.Cells[3, "M"] = TypeGO.SelectedItem.ToString().Remove(0, 38) + NumberBR.SelectedItem.ToString().Remove(0, 38) + " " +
+                        TypeConnect.SelectedItem.ToString().Remove(0, 38) + " " +
+                        ButDN.SelectedItem.ToString().Remove(0, 38) + " " + "PN" + ButPN.SelectedItem.ToString().Remove(0, 38) + " LN" +
+                        double.Parse(TextLN.Text) / 1000;
+                }
+                WorkShEx.Cells[3, "P"] = "изд";
+                WorkShEx.Cells[3, "U"] = "Металлорукав";
+                WorkShEx.Cells[3, "W"] = Int16.Parse(NumbMetHos.Text);
+
+                //WorkShEx.Cells[8, "H"] = "text "+TypeConnect.SelectedItem.ToString().Remove(0,38);
+            }
         }
-
-
+        private void checked_true(object sender, RoutedEventArgs e)
+        {
+            CustLabeling.IsEnabled = true;
+        }
+        private void checked_false(object sender, RoutedEventArgs e)
+        {
+            CustLabeling.IsEnabled = false;
+        }
     }
     
 }
